88402c9060989fde39227b2a2932ebb8c379cf22
venkata-cm <venkata@civilmaps.com>
2017-07-17 22:59:30 +0530

    added script

---
+++ 201-vmss-ubuntu-autoscale/deploy_dependencies.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# exit on any error
+set -e
+
+help()
+{
+
+    echo "This script install Jenkins master on an Ubuntu VM image"
+    echo "Parameters:"
+    echo "-n number of slave nodes to configure on the master"
+    echo "-h view this help content"
+}
+
+#Log method to control/redirect log output
+log()
+{
+    echo "$1"
+}
+
+
+
+log "Begin VMSS install script"
+
+
+# Install openjdk-7
+install_java()
+{
+    log "Installing openjdk-7"
+    apt-get -y update 
+    apt-get -y install openjdk-7-jdk 
+    apt-get -y update 
+}
+
+
+# Primary Install Tasks
+install_java >> deploy.log
+
+exit 0
+
+
+
+
