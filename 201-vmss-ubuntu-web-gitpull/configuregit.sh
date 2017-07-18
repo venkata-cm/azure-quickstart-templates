ccb8deac09d100bbe12d5d45fea49624f6db5512
venkata-cm <venkata@civilmaps.com>
2017-07-18 06:10:55 +0530

    trying vmss

--- 201-vmss-ubuntu-web-gitpull/configuregit.sh
+++ 201-vmss-ubuntu-web-gitpull/configuregit.sh
@@ -4,39 +4,4 @@
 set -e
 
 echo "Welcome to configuressl.sh"
-echo "Number of parameters was: " $#
-
-help()
-{
-
-    echo "This script install Jenkins master on an Ubuntu VM image"
-    echo "Parameters:"
-    echo "-n number of slave nodes to configure on the master"
-    echo "-h view this help content"
-}
-
-#Log method to control/redirect log output
-log()
-{
-    echo "$1"
-}
-
-
-
-log "Begin VMSS install script"
-
-
-# Install openjdk-7
-install_java()
-{
-    log "Installing openjdk-7"
-    apt-get -y update 
-    apt-get -y install openjdk-7-jdk 
-    apt-get -y update 
-}
-
-
-# Primary Install Tasks
-install_java
-
-exit 0
+echo "Done!"
